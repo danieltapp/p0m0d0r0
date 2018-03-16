4b31e4ed7b6f60ce0b169f2c3afc25ddf62d39c7
Daniel <danieltapp@gmail.com>
2018-03-16 14:55:33 -0400

    This is the 2nd fCC advanced front end project. https://codepen.io/danieltapp/full/eMzGBW/

---
+++ js/index.js
@@ -0,0 +1,49 @@
+let countdown;
+const timeDisplay = document.querySelector(".timeDisplay");
+const customTimeButton = document.querySelector(".customTimeButton");
+const pomodoroButton = document.querySelector(".pomodoroButton");
+const endButton = document.querySelector(".endButton");
+const customTimeInput = document.querySelector(".customTimeInput");
+
+function timer(seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTime(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTime(secondsLeft);
+  }, 1000);
+}
+
+function displayTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  timeDisplay.innerHTML = minutes + " : " + remainingSeconds;
+}
+
+customTimeButton.addEventListener("click", function() {
+  var customTime = customTimeInput.value * 60;
+  if (customTime <= 0) {
+    alert("You must enter at least 1 minute!");
+  } else {
+    timer(customTime);
+    customTimeInput.value = "";
+  }
+});
+
+pomodoroButton.addEventListener("click", function() {
+  timer(1500);
+});
+
+endButton.addEventListener("click", function() {
+  timer(0);
+  timeDisplay.innerHTML = "";
+});
