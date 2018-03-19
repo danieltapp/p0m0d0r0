b3deff1c6ecb0e989de5abf154ed31d63aac3d18
Daniel <danieltapp@gmail.com>
2018-03-19 13:53:55 -0400

    Added modal to define the pomodoro method for the user

--- js/index.js
+++ js/index.js
@@ -45,5 +45,19 @@ pomodoroButton.addEventListener("click", function() {
 
 endButton.addEventListener("click", function() {
   timer(0);
-  timeDisplay.innerHTML = "";
+  timeDisplay.innerHTML ="";
 });
+
+  $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      },
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+    }
+  );
+      
